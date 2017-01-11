2fd9fb1c68834bf8216a350d61d60c5fbf5735c3
inteltao <inteltao7@gmail.com>
2017-01-10 21:15:49 -0800

    various bug fix, add stop.sh

--- github.sh
+++ github.sh
@@ -6,7 +6,8 @@
 args=("$@")
 local_path=${args[0]}
 remote_url=${args[1]}
-refresh_time=5
+echo "remote_url is $remote_url"
+echo "local_path is $local_path"
 #First, redirect to the local path and check if this if the git repo is ready
 if [ ! -d "${local_path}" ]; then
 	mkdir "${local_path}"
@@ -16,7 +17,7 @@ if [ ! -d .git ]
 	then
 		echo "this is not a valid git repo"
 		echo "Trying to clone the repo..."
-		git clone "${remote_url}"
+		git clone "${remote_url}" .
 fi
 
 #Check if it has a valid origin
@@ -28,28 +29,32 @@ fi
 #		exit
 #fi
 
-#Then, we will set the refresh time of checking git status, default is 5 min 
-if [ -z "${args[2]}" ]
-	then
-		refresh_time=$(( refresh_time * 60 ))
-	else
-		refresh_time=$(( args[2] * 60 ))
-fi
+#Then, we will set the refresh time of checking git status, default is 5 min
+#deprecated 
+#if [ -z "${args[2]}" ]
+#	then
+#		refresh_time=$(( refresh_time * 60 ))
+#	else
+#		refresh_time=$(( args[2] * 60 ))
+#fi
 
 #Next, we will periodically check for the git status
+refresh_time=300
 current_date=$(date -u)
 while :
 do
 	echo "trying to auto-sync the repo..."
 	git pull "${remote_url}"
-	if [[ 'git status --porcelain' ]]
+	if [[ `git status --porcelain` ]]
 		then
 			echo "changes found, syncing..."
+			git status
+			exit
 			git add -A &> /dev/null
 			git commit -m "$current_date" &> /dev/null
 			git push "${remote_url}" &> /dev/null
 	fi
-	sleep $refresh_time
+	sleep 500
 done
 
 
